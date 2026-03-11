5c2149fdeeee4c61ce89554eedbd11d579022ce5
Serguei E. Leontiev <leo@sai.msu.ru>
2026-03-11 10:34:07 +0300

    lcc-1.29 (#3)
    * Add version lcc-1.29;
    * Add version qemu-user-1.1 (qemu_e2k-1.1);
    * Improved cache to `restore-save`;
    * Check downloads integrity by SHA512.

---
+++ tests/simple_false.c
@@ -0,0 +1,9 @@
+// vim:set sw=4 ts=8 et fileencoding=utf8:
+// SPDX-License-Identifier: BSD-2-Clause
+// SPDX-FileCopyrightText: 2025 Сергей Леонтьев (leo@sai.msu.ru)
+
+#include <stdlib.h>
+
+int main(void) {
+    return EXIT_FAILURE;
+}
